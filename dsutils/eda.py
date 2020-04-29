8676ac34750dec25c6860eede06a480fec0c1f3f
Ritvik19 <rastogiritvik99@gmail.com>
2020-04-29 18:11:55 +0530

    Advanced Features

--- dsutils/eda.py
+++ dsutils/eda.py
@@ -22,16 +22,17 @@ def feature_distribution(data, feature, target):
         'mean': [],
         'std' : []
     }
-    for i in sorted(data[target].unique()):
-        description['min'].append(round(data[data[target] == i][feature].min(), 2))
-        description['1st'].append(round(data[data[target] == i][feature].quantile(0.01), 2))
-        description['25th'].append(round(data[data[target] == i][feature].quantile(0.25), 2))
-        description['50th'].append(round(data[data[target] == i][feature].quantile(0.50), 2))
-        description['75th'].append(round(data[data[target] == i][feature].quantile(0.75), 2))
-        description['99th'].append(round(data[data[target] == i][feature].quantile(0.99), 2))
-        description['max'].append(round(data[data[target] == i][feature].max(), 2))
-        description['mean'].append(round(data[data[target] == i][feature].mean(), 2))
-        description['std'].append(round(data[data[target] == i][feature].std(), 2))
+    if target is not None:
+        for i in sorted(data[target].unique()):
+            description['min'].append(round(data[data[target] == i][feature].min(), 2))
+            description['1st'].append(round(data[data[target] == i][feature].quantile(0.01), 2))
+            description['25th'].append(round(data[data[target] == i][feature].quantile(0.25), 2))
+            description['50th'].append(round(data[data[target] == i][feature].quantile(0.50), 2))
+            description['75th'].append(round(data[data[target] == i][feature].quantile(0.75), 2))
+            description['99th'].append(round(data[data[target] == i][feature].quantile(0.99), 2))
+            description['max'].append(round(data[data[target] == i][feature].max(), 2))
+            description['mean'].append(round(data[data[target] == i][feature].mean(), 2))
+            description['std'].append(round(data[data[target] == i][feature].std(), 2))
     description['min'].append(round(data[feature].min(), 2))
     description['1st'].append(round(data[feature].quantile(0.01), 2))
     description['25th'].append(round(data[feature].quantile(0.25), 2))
@@ -42,7 +43,10 @@ def feature_distribution(data, feature, target):
     description['mean'].append(round(data[feature].mean(), 2))
     description['std'].append(round(data[feature].std(), 2))        
     description = pd.DataFrame(description)
-    description.index = list(sorted(data[target].unique()))+ ['overall']
+    if target is not None:
+        description.index = list(sorted(data[target].unique()))+ ['overall']
+    else:
+        description.index = ['overall']
     return description
 
 def plot_feature_distribution(data, feature, target, labels=None, lthreshold=None, uthreshold=None):
@@ -62,14 +66,15 @@ def plot_feature_distribution(data, feature, target, labels=None, lthreshold=Non
     sns.distplot(data[feature], ax=ax[0])
     ax[0].grid()
 
-    lthreshold = data[feature] >= lthreshold if lthreshold is not None else True
-    uthreshold = data[feature] <= uthreshold if uthreshold is not None else True
-    labels = sorted(data[target].unique()) if labels is None else labels
-    
-    ax[1].set_title(f'Classwise Distribution of "{feature}"')
-    
-    for i in range(len(labels)):
-        sns.distplot(data[(data[target] == i) & (lthreshold) & (uthreshold)][feature], ax=ax[1], label=labels[i])
-    ax[1].grid()
-    ax[1].legend()    
+    if target is not None:
+        lthreshold = data[feature] >= lthreshold if lthreshold is not None else True
+        uthreshold = data[feature] <= uthreshold if uthreshold is not None else True
+        labels = sorted(data[target].unique()) if labels is None else labels
+
+        ax[1].set_title(f'Classwise Distribution of "{feature}"')
+
+        for i in range(len(labels)):
+            sns.distplot(data[(data[target] == i) & (lthreshold) & (uthreshold)][feature], ax=ax[1], label=labels[i])
+        ax[1].grid()
+        ax[1].legend()    
     return fig, ax
